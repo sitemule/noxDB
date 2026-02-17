365ae8c97f9141d655f883c3830991beab2322aa
Niels Liisberg <nli@system-method.com>
2026-02-17 17:28:58 +0100

    sql suppport for "final table" when not journaled

--- src/sqlio.c
+++ src/sqlio.c
@@ -760,7 +760,8 @@ PJXSQL jx_sqlOpen(PUCHAR sqlstmt , PJXNODE pSqlParmsP, LONG formatP , LONG start
    // and IBM i does not support statement attribute to set the pr statement. :/
    // so we simply append the "with ur" uncommited read options
    if ((0 != memicmp ( sqlTempStmt , "call"  , 4))
-   &&  (0 != memicmp ( sqlTempStmt , "values", 6))) {
+   &&  (0 != memicmp ( sqlTempStmt , "values", 6))
+   &&  (NULL == stristr ( sqlTempStmt , " final table"))) {
       PUCHAR lookFrom = finalSQLPart (sqlTempStmt);
       static BOOL compilereg = true;
       static regex_t hasLimitReg;
